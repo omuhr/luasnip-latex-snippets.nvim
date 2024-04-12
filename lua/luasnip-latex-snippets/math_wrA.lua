95b4c86671c25017ebc4b4ae74cdda41050e2b4c
muoscar <oscar.muhr@gmail.com>
2024-04-12 15:44:29 +0200

    Some more trailing whitespaces removed.

--- lua/luasnip-latex-snippets/math_wrA.lua
+++ lua/luasnip-latex-snippets/math_wrA.lua
@@ -38,7 +38,11 @@ local frac_node = {
     if depth ~= 0 then
       return string.format("%s\\frac{}", stripped)
     else
-      return string.format("%s\\frac{%s}", stripped:sub(1, i - 1), stripped:sub(i + 1, #stripped - 1))
+      return string.format(
+        "%s\\frac{%s}",
+        stripped:sub(1, i - 1),
+        stripped:sub(i + 1, #stripped - 1)
+      )
     end
   end, {}),
   t("{"),
